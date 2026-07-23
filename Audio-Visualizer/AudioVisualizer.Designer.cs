5274414f227a5f2f64652350b9cd02e8de014677
senay <senay@OMER>
2026-07-23 20:33:13 +0300

    Proje dosyası ekle.

---
+++ Audio-Visualizer/AudioVisualizer.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace AudioVisualizer
+{
+    partial class AudioVisualizer
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AudioVisualizer));
+            this.mainpanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses12pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses10pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses8pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses11pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses6pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses9pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses4pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses7pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses2pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses5pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses3pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.ses1pnl = new Guna.UI2.WinForms.Guna2Panel();
+            this.kapabtn = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.dragctrl = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.kclbtn = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.mainpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainpanel.BackColor = System.Drawing.Color.Transparent;
+            this.mainpanel.BorderRadius = 10;
+            this.mainpanel.Controls.Add(this.ses12pnl);
+            this.mainpanel.Controls.Add(this.ses10pnl);
+            this.mainpanel.Controls.Add(this.ses8pnl);
+            this.mainpanel.Controls.Add(this.ses11pnl);
+            this.mainpanel.Controls.Add(this.ses6pnl);
+            this.mainpanel.Controls.Add(this.ses9pnl);
+            this.mainpanel.Controls.Add(this.ses4pnl);
+            this.mainpanel.Controls.Add(this.ses7pnl);
+            this.mainpanel.Controls.Add(this.ses2pnl);
+            this.mainpanel.Controls.Add(this.ses5pnl);
+            this.mainpanel.Controls.Add(this.ses3pnl);
+            this.mainpanel.Controls.Add(this.ses1pnl);
+            this.mainpanel.FillColor = System.Drawing.Color.Gray;
+            this.mainpanel.Location = new System.Drawing.Point(20, 20);
+            this.mainpanel.Margin = new System.Windows.Forms.Padding(0);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(660, 440);
+            this.mainpanel.TabIndex = 0;
+            // 
+            // ses12pnl
+            // 
+            this.ses12pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses12pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses12pnl.BorderColor = System.Drawing.Color.Transparent;
+            this.ses12pnl.BorderRadius = 5;
+            this.ses12pnl.FillColor = System.Drawing.Color.White;
+            this.ses12pnl.Location = new System.Drawing.Point(602, 412);
+            this.ses12pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses12pnl.Name = "ses12pnl";
+            this.ses12pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses12pnl.TabIndex = 11;
+            // 
+            // ses10pnl
+            // 
+            this.ses10pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses10pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses10pnl.BorderRadius = 5;
+            this.ses10pnl.FillColor = System.Drawing.Color.White;
+            this.ses10pnl.Location = new System.Drawing.Point(494, 412);
+            this.ses10pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses10pnl.Name = "ses10pnl";
+            this.ses10pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses10pnl.TabIndex = 9;
+            // 
+            // ses8pnl
+            // 
+            this.ses8pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses8pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses8pnl.BorderRadius = 5;
+            this.ses8pnl.FillColor = System.Drawing.Color.White;
+            this.ses8pnl.Location = new System.Drawing.Point(386, 412);
+            this.ses8pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses8pnl.Name = "ses8pnl";
+            this.ses8pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses8pnl.TabIndex = 7;
+            // 
+            // ses11pnl
+            // 
+            this.ses11pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses11pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses11pnl.BorderRadius = 5;
+            this.ses11pnl.FillColor = System.Drawing.Color.White;
+            this.ses11pnl.Location = new System.Drawing.Point(548, 412);
+            this.ses11pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses11pnl.Name = "ses11pnl";
+            this.ses11pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses11pnl.TabIndex = 10;
+            // 
+            // ses6pnl
+            // 
+            this.ses6pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses6pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses6pnl.BorderRadius = 5;
+            this.ses6pnl.FillColor = System.Drawing.Color.White;
+            this.ses6pnl.Location = new System.Drawing.Point(278, 412);
+            this.ses6pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses6pnl.Name = "ses6pnl";
+            this.ses6pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses6pnl.TabIndex = 5;
+            // 
+            // ses9pnl
+            // 
+            this.ses9pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses9pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses9pnl.BorderRadius = 5;
+            this.ses9pnl.FillColor = System.Drawing.Color.White;
+            this.ses9pnl.Location = new System.Drawing.Point(440, 412);
+            this.ses9pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses9pnl.Name = "ses9pnl";
+            this.ses9pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses9pnl.TabIndex = 8;
+            // 
+            // ses4pnl
+            // 
+            this.ses4pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses4pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses4pnl.BorderRadius = 5;
+            this.ses4pnl.FillColor = System.Drawing.Color.White;
+            this.ses4pnl.Location = new System.Drawing.Point(170, 412);
+            this.ses4pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses4pnl.Name = "ses4pnl";
+            this.ses4pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses4pnl.TabIndex = 3;
+            // 
+            // ses7pnl
+            // 
+            this.ses7pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses7pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses7pnl.BorderRadius = 5;
+            this.ses7pnl.FillColor = System.Drawing.Color.White;
+            this.ses7pnl.Location = new System.Drawing.Point(332, 412);
+            this.ses7pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses7pnl.Name = "ses7pnl";
+            this.ses7pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses7pnl.TabIndex = 6;
+            // 
+            // ses2pnl
+            // 
+            this.ses2pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses2pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses2pnl.BorderRadius = 5;
+            this.ses2pnl.FillColor = System.Drawing.Color.White;
+            this.ses2pnl.Location = new System.Drawing.Point(62, 412);
+            this.ses2pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses2pnl.Name = "ses2pnl";
+            this.ses2pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses2pnl.TabIndex = 1;
+            // 
+            // ses5pnl
+            // 
+            this.ses5pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses5pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses5pnl.BorderRadius = 5;
+            this.ses5pnl.FillColor = System.Drawing.Color.White;
+            this.ses5pnl.Location = new System.Drawing.Point(224, 412);
+            this.ses5pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses5pnl.Name = "ses5pnl";
+            this.ses5pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses5pnl.TabIndex = 4;
+            // 
+            // ses3pnl
+            // 
+            this.ses3pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ses3pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses3pnl.BorderRadius = 5;
+            this.ses3pnl.FillColor = System.Drawing.Color.White;
+            this.ses3pnl.Location = new System.Drawing.Point(116, 412);
+            this.ses3pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses3pnl.Name = "ses3pnl";
+            this.ses3pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses3pnl.TabIndex = 2;
+            // 
+            // ses1pnl
+            // 
+            this.ses1pnl.BackColor = System.Drawing.Color.Transparent;
+            this.ses1pnl.BorderRadius = 5;
+            this.ses1pnl.FillColor = System.Drawing.Color.White;
+            this.ses1pnl.Location = new System.Drawing.Point(8, 412);
+            this.ses1pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.ses1pnl.Name = "ses1pnl";
+            this.ses1pnl.Size = new System.Drawing.Size(50, 20);
+            this.ses1pnl.TabIndex = 0;
+            // 
+            // kapabtn
+            // 
+            this.kapabtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.kapabtn.FillColor = System.Drawing.Color.Transparent;
+            this.kapabtn.IconColor = System.Drawing.Color.Black;
+            this.kapabtn.Location = new System.Drawing.Point(680, 0);
+            this.kapabtn.Name = "kapabtn";
+            this.kapabtn.Size = new System.Drawing.Size(20, 20);
+            this.kapabtn.TabIndex = 1;
+            this.kapabtn.Click += new System.EventHandler(this.kapabtn_Click);
+            // 
+            // dragctrl
+            // 
+            this.dragctrl.DockIndicatorColor = System.Drawing.Color.Transparent;
+            this.dragctrl.DockIndicatorTransparencyValue = 0.3D;
+            this.dragctrl.DragEndTransparencyValue = 0.3D;
+            this.dragctrl.DragStartTransparencyValue = 0.3D;
+            this.dragctrl.TargetControl = this;
+            this.dragctrl.UseTransparentDrag = true;
+            // 
+            // kclbtn
+            // 
+            this.kclbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.kclbtn.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.kclbtn.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.kclbtn.FillColor = System.Drawing.Color.Transparent;
+            this.kclbtn.IconColor = System.Drawing.Color.Black;
+            this.kclbtn.Location = new System.Drawing.Point(660, 0);
+            this.kclbtn.Name = "kclbtn";
+            this.kclbtn.Size = new System.Drawing.Size(20, 20);
+            this.kclbtn.TabIndex = 3;
+            // 
+            // AudioVisualizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(700, 480);
+            this.Controls.Add(this.kclbtn);
+            this.Controls.Add(this.kapabtn);
+            this.Controls.Add(this.mainpanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(700, 480);
+            this.MinimumSize = new System.Drawing.Size(700, 480);
+            this.Name = "AudioVisualizer";
+            this.Opacity = 0.3D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Audio Visualizer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CoolBoy13_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CoolBoy13_FormClosed);
+            this.Load += new System.EventHandler(this.AudioVisualizer_Load);
+            this.mainpanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel mainpanel;
+        private Guna.UI2.WinForms.Guna2ControlBox kapabtn;
+        private Guna.UI2.WinForms.Guna2DragControl dragctrl;
+        private Guna.UI2.WinForms.Guna2ControlBox kclbtn;
+        private Guna.UI2.WinForms.Guna2Panel ses1pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses4pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses2pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses3pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses12pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses10pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses8pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses11pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses6pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses9pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses7pnl;
+        private Guna.UI2.WinForms.Guna2Panel ses5pnl;
+    }
+}
+
---
+++ Audio-Visualizer/AudioVisualizer.cs
@@ -0,0 +1,230 @@
+﻿using Guna.UI2.WinForms;
+using NAudio.Dsp;
+using NAudio.Wave;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AudioVisualizer
+{
+    public partial class AudioVisualizer : Form
+    {
+        private WasapiLoopbackCapture capture;
+        private const int fftLength = 1024;
+        private Complex[] fftBuffer = new Complex[fftLength];
+        private int fftPos = 0;
+        private float[] spectrumData = new float[fftLength / 2];
+        private const int BAR_COUNT = 12;
+        private Guna2Panel[] audioPanels = new Guna2Panel[BAR_COUNT];
+        private float[] currentHeights = new float[BAR_COUNT];
+        private int[] bandIndices = new int[BAR_COUNT + 1];
+        private int[] panelBottomPositions = new int[BAR_COUNT];
+        private const int MAX_PANEL_HEIGHT = 400;
+        private readonly float[] frequencyScales = new float[12]
+        {
+            3.5f,  
+            3.8f,  
+            2.8f,  
+            3.5f,  
+            4.5f,  
+            5.5f,  
+            7.0f,  
+            8.5f,  
+            10.0f, 
+            11.0f, 
+            13.0f, 
+            3.5f   
+        };
+        public AudioVisualizer()
+        {
+            InitializeComponent();     
+            FindPanels();
+            CalculateLogarithmicBands();
+            InitAudioCapture();
+
+            this.Location = new Point(120, 120);
+            Timer animTimer = new Timer();
+            animTimer.Interval = 16;
+            animTimer.Tick += AnimTimer_Tick;
+            animTimer.Start();
+        }
+
+        
+
+        private void FindPanels()
+        {
+            for (int i = 0; i < BAR_COUNT; i++)
+            {
+                string panelName = $"ses{i + 1}pnl";
+                Control[] found = this.Controls.Find(panelName, true);
+
+                if (found.Length > 0 && found[0] is Guna2Panel panel)
+                {
+                    audioPanels[i] = panel;
+                    panelBottomPositions[i] = panel.Top + panel.Height;
+                    currentHeights[i] = panel.Height;
+                }
+                else
+                {
+                    MessageBox.Show($"{panelName} isimli Guna2Panel bulunamadı!");
+                }
+            }
+        }
+
+        private void CalculateLogarithmicBands()
+        {
+            int maxIndex = fftLength / 2;
+            double minFreq = 1;
+            double maxFreq = maxIndex;
+
+            for (int i = 0; i <= BAR_COUNT; i++)
+            {
+                double logIndex = minFreq * Math.Pow(maxFreq / minFreq, (double)i / BAR_COUNT);
+                bandIndices[i] = Math.Min((int)logIndex, maxIndex);
+            }
+            bandIndices[10] = 220; 
+            bandIndices[11] = 380; 
+            bandIndices[12] = 512;
+        }
+
+        private void InitAudioCapture()
+        {
+            capture = new WasapiLoopbackCapture();
+
+            capture.DataAvailable += (s, e) =>
+            {
+                for (int i = 0; i < e.BytesRecorded; i += 4)
+                {
+                    float sample = BitConverter.ToSingle(e.Buffer, i);
+
+                    fftBuffer[fftPos].X = sample;
+                    fftBuffer[fftPos].Y = 0;
+                    fftPos++;
+
+                    if (fftPos >= fftLength)
+                    {
+                        fftPos = 0;
+                        FastFourierTransform.FFT(true, (int)Math.Log(fftLength, 2), fftBuffer);
+
+                        for (int j = 0; j < fftLength / 2; j++)
+                        {
+                            spectrumData[j] = (float)Math.Sqrt(fftBuffer[j].X * fftBuffer[j].X + fftBuffer[j].Y * fftBuffer[j].Y);
+                        }
+                    }
+                }
+            };
+
+            capture.StartRecording();
+        }
+
+        private void AnimTimer_Tick(object sender, EventArgs e)
+        {
+            float attackSpeed = 0.50f; 
+            float decaySpeed = 7.0f; 
+
+            for (int i = 0; i < BAR_COUNT; i++)
+            {
+                if (audioPanels[i] == null) continue;
+
+                int startBin = bandIndices[i];
+                int endBin = bandIndices[i + 1];
+                if (endBin <= startBin) endBin = startBin + 1;
+
+                float maxIntensity = 0;
+                for (int j = startBin; j < endBin; j++)
+                {
+                    if (spectrumData[j] > maxIntensity)
+                        maxIntensity = spectrumData[j];
+                }
+
+                if (maxIntensity < 0.005f)
+                    maxIntensity = 0;
+
+                float targetHeight = maxIntensity * 900f * frequencyScales[i];
+
+                if (targetHeight > MAX_PANEL_HEIGHT) targetHeight = MAX_PANEL_HEIGHT;
+
+                if (targetHeight > currentHeights[i])
+                {
+                    currentHeights[i] += (targetHeight - currentHeights[i]) * attackSpeed;
+                }
+                else
+                {
+                    currentHeights[i] -= decaySpeed;
+                    if (currentHeights[i] < 15) currentHeights[i] = 15;
+                }
+                UpdatePanelBounds(i, (int)currentHeights[i]);
+            }
+        }
+
+        private void UpdatePanelBounds(int index, int newHeight)
+        {
+            Guna2Panel panel = audioPanels[index];           
+            panel.Height = newHeight;
+            panel.Top = panelBottomPositions[index] - newHeight;
+        }
+
+        private void AudioVisualizer_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.Control)
+            {
+                
+                Rectangle workingArea = Screen.FromControl(this).WorkingArea;
+                int padding = 12;
+                int padding2 = 120;
+
+                switch (e.KeyCode)
+                {
+                    case Keys.Left: // Ekranın en soluna yasla (Yüksekliği koru)
+                        this.Left = workingArea.Left + padding;
+                        e.Handled = true; // Tuş vuruşunu işlendi olarak işaretle
+                        break;
+
+                    case Keys.Right: // Ekranın en sağına yasla (Yüksekliği koru)
+                        this.Left = workingArea.Right - this.Width - padding;
+                        e.Handled = true;
+                        break;
+
+                    case Keys.Up: // Ekranın en üstüne yasla (Yatay konumu koru)
+                        this.Top = workingArea.Top + padding;
+                        e.Handled = true;
+                        break;
+
+                    case Keys.Down: // Ekranın en altına yasla (Yatay konumu koru)
+                        this.Top = workingArea.Bottom - this.Height - padding;
+                        e.Handled = true;
+                        break;
+
+                    case Keys.Space:
+                        this.Top = workingArea.Top + padding2;
+                        this.Left = workingArea.Left + padding2;
+                        e.Handled = true;
+                        break;
+
+                }
+            }
+        }
+
+        private void kapabtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CoolBoy13_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CoolBoy13_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AudioVisualizer_Load(object sender, EventArgs e)
+        {
+           
+            this.KeyDown += AudioVisualizer_KeyDown;
+        }
+    }
+}
